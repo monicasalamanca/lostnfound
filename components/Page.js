24e8bdd08e02978269426cf1650605124075b35c
Monica Salamanca <moni.salamanca@gmail.com>
2019-10-26 18:31:09 -0400

    created nav and reset styled component

--- components/Page.js
+++ components/Page.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../components/Header';
 import Meta from '../components/Meta';
+import CSSReset from './styles/cssReset';
 
 class Page extends React.Component {
   render() { 
     return ( 
       <div>
+        <CSSReset />
         <Meta />
         <Header />
         {this.props.children}
